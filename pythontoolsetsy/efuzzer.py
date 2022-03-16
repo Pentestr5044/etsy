b5a2379d1f222b0aa6bae00fa0e730890b0b582d
Pentestr5044 <65639873+Pentestr5044@users.noreply.github.com>
2022-03-15 19:05:15 -0700

    fixed
    probably fixed

--- pythontoolsetsy/efuzzer.py
+++ pythontoolsetsy/efuzzer.py
@@ -1,19 +1,12 @@
 #imports first
 import time
-import threading
 import urllib3
 import certifi
-import argparse
 #define argparse argument for threads
-parser = argparse.ArgumentParser(description='Etsy attack tool')
-parser.add_argument("-t", "--threadsN", type=int,
-                    help='number for thread count')
-args = parser.parse_args()
 #define variables
 http = urllib3.PoolManager(ca_certs=certifi.where())
 url = "https://www.etsy.com/"
 words = []
-threads = args.threadsN
 assetFuzz = "./assetnoteslist.txt"
 wordlistBase3 = "./actionwords.txt"
 wordlistBase1 = "./massivewordlist.txt"
@@ -51,36 +44,37 @@ def wordlist_builder(wordlistBase1, wordlistBase3, directories1, assetFuzz):
 
 attack_field = wordlist_builder(wordlistBase1, wordlistBase3, directories1, assetFuzz)
 #headers = open("./useragents.txt", 'r')
-
-
-def directoryBust(arg):
-    attacks = []
-    this = attack_field
-    for i in this:
-        this1 = str(i).strip('b\'')
-        this2 = this1.strip('\\r\\n')
-        attacks.append(this2)
-    for attempts in attacks:
-            #for header in headers:
-             try:
-                r = http.request("GET",url+attempts, retries=False)
-                if r.status == 200 or 403 or 500:
+v = 0
+#function 3 iteration and threading
+if len(attack_field) > 0:
+    if v < len(attack_field):
+        attacks = []
+        this = attack_field
+        for i in this:
+            this1 = str(i).strip('b\'')
+            this2 = this1.strip('\\r\\n')
+            attacks.append(this2)
+        for attempts in attacks:
+            url1 = url + attempts
+        # for header in headers:
+            try:
+                r = http.request("GET", url1, retries=False)
+                if r.status == 200:
                     print("found")
                     print(r.status)
-                    print(r.data)
-             except urllib3.exceptions.NewConnectionError:
+                    print(url1)
+                elif r.status == 403:
+                    print("found")
+                    print(r.status)
+                    print(url1)
+                elif r.status == 500:
+                    print("found")
+                    print(r.status)
+                    print(url1)
+                else:
+                    print()
+            except urllib3.exceptions.NewConnectionError:
                 print()
-             time.sleep(2)
-    pass
-
-
-
-
-
-
-
-#function 3 iteration and threading
-for i in range(threads):
-   t = threading.Thread(target=directoryBust(attack_field))
-   t.start()
-   time.sleep(2)
+                time.sleep(2)
+    time.sleep(5)
+    v += 1
