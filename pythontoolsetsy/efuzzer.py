3f867aea60dd7f5c84828ab47f02d32286ec57cf
Pentestr5044 <65639873+Pentestr5044@users.noreply.github.com>
2022-03-21 14:30:58 -0700

    taadaa
    taaadaaa

--- pythontoolsetsy/efuzzer.py
+++ pythontoolsetsy/efuzzer.py
@@ -5,12 +5,7 @@ import certifi
 #define argparse argument for threads
 #define variables
 http = urllib3.PoolManager(ca_certs=certifi.where())
-url = "https://www.etsy.com/"
-words = []
-assetFuzz = "./assetnoteslist.txt"
-wordlistBase3 = "./actionwords.txt"
-wordlistBase1 = "./massivewordlist.txt"
-directories1 = "./directories.txt"
+url = input("enter url: ")
 #user agent randomized per request
 #Origin header to cycle through proxy IP, localhost IP, and target ip range.
 
@@ -18,31 +13,9 @@ directories1 = "./directories.txt"
 #function 1 wordlist definition/generator
 
 
-def wordlist_builder(wordlistBase1, wordlistBase3, directories1, assetFuzz):
-    wf3 = open(assetFuzz, "rb")
-    wf1 = open(wordlistBase3, "rb")
-    wf = open(wordlistBase1, "rb")
-    wf2 = open(directories1, "rb")
-    fuzz = wf3.readlines()
-    baseWords = wf.readline()
-    actionWords = wf1.readlines()
-    directories = wf2.readlines()
-    for i in fuzz:
-        words.append(i)
-    wf3.close()
-    for i in baseWords:
-        words.append(i)
-    wf.close()
-    for i in actionWords:
-        words.append(i)
-    wf1.close()
-    for i in directories:
-        words.append(i)
-    wf2.close()
-    return words
-
-
-attack_field = wordlist_builder(wordlistBase1, wordlistBase3, directories1, assetFuzz)
+attack_field0 = input("please enter file location: ")
+attack_field1 = open(attack_field0,"rb")
+attack_field = attack_field1.readlines()
 #headers = open("./useragents.txt", 'r')
 v = 0
 #function 3 iteration and threading
